db771c7270311d30b19a9fdd7824c9f61b521eeb
贾明盛 <jms1209@qq.com>
2018-01-16 15:07:50 +0800

    Initial commit

---
+++ chapter01/read.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a decimal number: ";
+    cin >> number;
+    cout << "The number you entered is " << number << "." << endl;
+    return 0;
+}
